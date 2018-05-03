c392a583a8876164922814ec926b18e41ee15f8f
David Numan <david.numan@civicactions.com>
2018-05-02 23:10:19 -0400

    Not setting GID because it breaks if group does not exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# To avoid file permission conflicts change the "node" user to the UID/GID of
+# To avoid file permission conflicts change the "node" user to the UID of
 # the owner of the working directory and run the command as the "node" user.
-usermod -o -u $(stat -c '%u' .) -g $(stat -c '%g' .) -s /bin/bash -d /home/node/app node
+usermod -o -u $(stat -c '%u' .) -s /bin/bash -d /home/node/app node
 
 # To ensure the node path is maintained and quoting arbitrary strings,
 # run the user command via a temporary script.
